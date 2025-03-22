4d048e9a0799db3b43321b1c61b968eeb9427cb4
George Panaretos <georgepanaretos.cac@gmail.com>
2025-03-22 23:51:53 +0100

    GPS Data Dto

--- src/main/java/com/truck/gps/truck/gps/dto/GpsDataDto.java
+++ src/main/java/com/truck/gps/truck/gps/dto/GpsDataDto.java
@@ -1,4 +1,10 @@
 package com.truck.gps.truck.gps.dto;
 
+import lombok.Data;
+
+@Data
 public class GpsDataDto {
+    private String truckId;
+    private double latitude;
+    private double longitude;
 }
