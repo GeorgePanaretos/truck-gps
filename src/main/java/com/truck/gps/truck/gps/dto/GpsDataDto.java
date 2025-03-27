4a158a7551763f527fcec29106a56a95b49a18bd
George Panaretos <georgepanaretos.cac@gmail.com>
2025-03-27 16:59:38 +0100

    Modify DTO

--- src/main/java/com/truck/gps/truck/gps/dto/GpsDataDto.java
+++ src/main/java/com/truck/gps/truck/gps/dto/GpsDataDto.java
@@ -1,8 +1,10 @@
 package com.truck.gps.truck.gps.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class GpsDataDto {
     private TruckDto truckId;
     private double latitude;
--- src/main/java/com/truck/gps/truck/gps/dto/TruckDto.java
+++ src/main/java/com/truck/gps/truck/gps/dto/TruckDto.java
@@ -1,8 +1,12 @@
 package com.truck.gps.truck.gps.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TruckDto {
     private String truckId;
     private String licensePlate;
